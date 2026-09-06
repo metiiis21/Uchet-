52b505b6b49fe353a47ffe0cd6de4c683bd2d55e
metiiis21 <kvasnikovdima@yandex.ru>
2026-09-06 22:08:34 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Кэш приложения. Меняйте VERSION при каждом обновлении index.html —
    иначе айфон продолжит показывать старую копию. */
-const VERSION = "uchet-2026-v1";
+const VERSION = "uchet-2026-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest",
                 "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
